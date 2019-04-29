1b26da5a8c8eb538abdb8aa90fa90eb3733fa18e
John <john.white@ngic.com>
2019-04-29 16:02:51 -0400

    Add files via upload

---
+++ oracle_regex_cleaning_phone_numbers.sql
@@ -0,0 +1,23 @@
+/*  clean up common phone number formatting and 
+    quality issues
+*/
+with
+------------------------------------------------
+base as (
+        select '(000) 000-0000' as phone_number from dual
+        union
+        select '9999999999' as phone_number from dual
+        union
+        select '0000000000' as phone_number from dual
+        union
+        select '(555) 555-9110' as phone_number from dual
+        union
+        select '5555559110' as phone_number from dual
+        )
+------------------------------------------------
+select
+b.phone_number,
+regexp_replace(b.phone_number,'[^0-9A-Za-z]|0+|9999999999', '') AS clean_phone
+
+from
+base b
